1cdb3500a16c46e5878ba6c772efd4618b72cd12
Arktetra <sameershrestha.sm@gmail.com>
2025-03-14 21:49:39 +0545

    Updates

---
+++ _app/immutable/entry/start.Cw-Ak92X.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/BnTnQbeR.js";export{t as start};
--- src/lib/config.ts
+++
@@ -1,5 +0,0 @@
-import { dev } from '$app/environment';
-
-export const title = 'Neural Flux';
-export const description = 'Blogs for ...';
-export const url = dev ? 'http://localhost:5173/' : '';
